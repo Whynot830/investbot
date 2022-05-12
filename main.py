ec540f3e788483debe946119ad6d49f8646859e1
Amin <aminfury@mail.ru>
2022-05-12 23:21:09 +0300

    1

--- main.py
+++ main.py
@@ -1,104 +1,192 @@
 import telebot
 import json
 import requests
-from telebot import types
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 import os
+
 bot = telebot.TeleBot("5279674502:AAFXF-kDxk_WVVo9Br5YN5PmNQohsR3oxFQ")
+
+def menu1():
+    m = InlineKeyboardMarkup()
+    m.row_width = 1
+    m.add(InlineKeyboardButton("📈 Цены", callback_data='getprice'),
+          InlineKeyboardButton("💼⟵ Добавить в портфель", callback_data='addtocase'),
+          InlineKeyboardButton("💼⟶ Получить портфель", callback_data='getcase'),
+          InlineKeyboardButton("🚫 Очистить портфель", callback_data='clear')
+          )
+    return m
+
+
+def menu2():
+    m = InlineKeyboardMarkup()
+    m.row_width = 1
+    m.add(InlineKeyboardButton("‹ В меню", callback_data="tomenu"))
+    return m
+
+@bot.message_handler(func=lambda message: message.text == 'hasbik')
+def hasbik(message):
+        bot.send_sticker(message.chat.id,sticker='CAACAgIAAxkBAAEEtqJifWXu_34d0o6GkVsEpaoOGEPgAQACkBkAAqfpQUpLQGC5lMd-WSQE')
+
+@bot.message_handler(func=lambda message: message.text == "/start")
+def mainmenu(message):
+    bot.send_message(message.chat.id,
+                     '"Цены" - получение цены котировки\n"Добавить в портфель" - добавление котировки в портфель'
+                     '\n"Получить портфель" - вывод портфеля и информации о прибыли', reply_markup=menu1())
+
+@bot.message_handler(func=lambda message: message.text != "/start")
+def errormsg(message):
+    bot.send_message(message.chat.id, "Перемещайтесь по меню с помощью встроенных кнопок")
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == "getprice":
+                bot.edit_message_text('Введите тикер для получения информации',call.message.chat.id,call.message.message_id)
+                bot.answer_callback_query(call.id)
+                bot.register_next_step_handler(call.message, send_ticker)
+            elif call.data == "addtocase":
+                c = get_count_of_stocks_case()
+                if (c<5):
+                    bot.edit_message_text("Введите тикер для добавления в портфель", call.message.chat.id,
+                                      call.message.message_id)
+                    bot.answer_callback_query(call.id)
+                    bot.register_next_step_handler(call.message, add_stock_case)
+                else:
+                    bot.edit_message_text("К сожалению, в текущей версии бота размер портфеля ограничен 5 позициями",
+                                          call.message.chat.id,call.message.message_id,reply_markup=menu2())
+            elif call.data == "getcase":
+                bot.edit_message_text("Подсчитываю...",call.message.chat.id,call.message.message_id)
+                bot.answer_callback_query(call.id)
+                get_stock_case(call.message)
+            elif call.data == "tomenu":
+                bot.edit_message_text(
+                    '"Цены" - получение цены котировки\n"Добавить в портфель" - добавление котировки в портфель'
+                    '\n"Получить портфель" - вывод портфеля и информации о прибыли',
+                    call.message.chat.id, call.message.message_id, reply_markup=menu1())
+                bot.answer_callback_query(call.id)
+            elif call.data == "clear":
+                clear_case()
+                bot.edit_message_text("Данные портфеля очищены\n",call.message.chat.id,call.message.message_id,reply_markup=menu2())
+    except Exception as e:
+        print(repr(e))
 
 def get_current_price(id):
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + id + '&interval=1min&apikey=GA64HKYKKJO0LUUU'
     try:
         r = requests.get(url)
         data = r.json()
-        print(data)
+        if 'Note' in data: return (-2)
         r1 = data['Time Series (1min)'][data['Meta Data']['3. Last Refreshed']]['4. close']
         res = id + ' ' + r1
         return (res.split(' '))
     except:
         return (-1)
 
-def catalog_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    Help = types.KeyboardButton(text="/help")
-    Price = types.KeyboardButton(text="/get_price")
-    Add_stock = types.KeyboardButton(text="/add_to_case")
-    Get_stock = types.KeyboardButton(text="/get_case")
-    keyboard.add(Help, Price, Add_stock, Get_stock)
-    return keyboard
-
-@bot.message_handler(content_types=['text'])
-def start(message):
-    if message.text == '/help':
-        bot.send_message(message.chat.id, 'Для получения цены котировки введите /get_price\nДля добавления котировки в портфель введите /add_to_case\nДля получения цены портфеля введите /get_case', reply_markup=catalog_keyboard())
-        #bot.send_message(message.from_user.id, 'Для получения цены котировки введите /get_price\nДля добавления котировки в портфель введите /add_to_case\nДля получения цены портфеля введите /get_case')
-    elif message.text == '/get_price':
-        bot.send_message(message.from_user.id, "input ticker")
-        bot.register_next_step_handler(message, send_ticker)  # следующий шаг – функция get_name
-    elif message.text == '/add_to_case':
-        bot.send_message(message.from_user.id, "input ticker")
-        bot.register_next_step_handler(message, add_stock_case)
-    elif message.text == '/get_case':
-        bot.send_message(message.from_user.id, "calculating")
-        get_stock_case(message)
-    else:
-        bot.send_message(message.from_user.id, 'Не смог разобрать команду.\nНапиши /help')
-
 def add_stock_case(message):
+    if (message.text == '/start'):
+        mainmenu(message)
+        return
     f = open('text.txt', 'a')
     id = message.text
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + id + '&interval=1min&apikey=GA64HKYKKJO0LUUU'
     try:
         t = get_current_price(id)
-        res = t[0]+' '+t[1]
-        if (not os.stat("text.txt").st_size == 0): res = '\n'+res
-        print(res)
+        if (t == -2):
+            bot.send_message(message.chat.id,
+                             "К сожалению, в данной версии бота присутствует ограничение, равное 5 запросов в минуту",
+                             reply_markup=menu2())
+            return
+        res = t[0] + ' ' + t[1]
+        if (not os.stat("text.txt").st_size == 0): res = '\n' + res
 
-        bot.send_message(message.from_user.id, "input amount of stocks")
+        bot.send_message(message.chat.id, "Введите количество")
         bot.register_next_step_handler(message, add_num_of_stocks, res)
     except:
-        bot.send_message(message.from_user.id, 'incorrect ticker. Try again')
-        bot.register_next_step_handler(message, add_stock_case)
+        bot.send_message(message.chat.id, 'Некорректный тикер.',reply_markup=menu2())
 
 def get_stock_case(message):
     f = open('text.txt', 'r')
-    summC=0
-    summS=0
-    while (True):
+    flag = True
+    summC = 0
+    summS = 0
+    msg = ''
+    if (os.stat("text.txt").st_size == 0):
+        bot.send_message(message.chat.id,"Данные по портфелю отсутствуют",reply_markup=menu2())
+        return
+    while (flag):
         s = f.readline().split(' ')
-        print(s)
         if (s == ''): break
         id = s[0]
         t = get_current_price(id)
-        if (t!=-1):
-            summC+=float(s[1])*float(s[2])
-            summS+=float(t[1])*float(s[2])
-            delta = (float(s[1]) - float(t[1])) * float(s[2])
-            if (delta>0): bot.send_message(message.from_user.id, id +': u have rised up ' + str(abs(delta)) +'USD ('+'{:8.3f}'.format(100*float(s[1]) / float(t[1])-100)+' %)')
-            else: bot.send_message(message.from_user.id, id +': u have lost ' + str(abs(delta)) +'USD ('+'{:8.3f}'.format(100*float(s[1]) / float(t[1])-100)+' %)')
-        else: break
-    bot.send_message(message.from_user.id, 'Итоговая разница в стоимости: '+ str(summC-summS) + 'USD ('+'{:8.3f}'.format(100*summC / summS-100)+' %)')
+        if (t != -1 and t!=-2):
+            summC += float(s[1]) * float(s[2])
+            summS += float(t[1]) * float(s[2])
+            delta = (float(t[1]) - float(s[1])) * float(s[2])
+            if (delta > 0):
+                msg += id + ': Вы заработали ' + '{:0.3f}'.format(abs(delta)) + ' USD (' + '{:0.1f}'.format(
+                    100 * float(t[1]) / float(s[1]) - 100) + ' %)'+'\n'
+            else:
+                msg += id + ': Вы потеряли ' + '{:0.3f}'.format(abs(delta)) + ' USD (' + '{:0.1f}'.format(
+                                     100 * float(t[1]) / float(s[1]) - 100) + ' %)'+'\n'
+        elif (t == -2 and msg==''):
+            flag = False
+            break
+        else:
+            break
+    if (msg!=''): bot.send_message(message.chat.id, msg)
+    if (flag):
+        bot.send_message(message.chat.id,
+                     'Итоговая разница в стоимости: ' + '{:0.3f}'.format(summS - summC) + ' USD (' + '{:0.1f}'.format(
+                         100 * summS / summC - 100) + ' %)',reply_markup=menu2())
+    else:
+        bot.send_message(message.chat.id,
+                         "К сожалению, в данной версии бота присутствует ограничение, равное 5 запросов в минуту",
+                         reply_markup=menu2())
 
 def send_ticker(message):
+    if (message.text == '/start'):
+        mainmenu(message)
+        return
     id = message.text
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + id + '&interval=1min&apikey=GA64HKYKKJO0LUUU'
     try:
         r = requests.get(url)
         data = r.json()
-        print(data)
-        res = 'Last refreshed information about ' + id + ' ticker at ' + data['Meta Data']['3. Last Refreshed'] + ':\n'
-        for r1 in (data['Time Series (1min)'][data['Meta Data']['3. Last Refreshed']]):
-            res = res + r1 + ' ' + data['Time Series (1min)'][data['Meta Data']['3. Last Refreshed']][r1] + '\n'
-        bot.send_message(message.from_user.id, res)
+        if 'Note' in data:
+            bot.send_message(message.chat.id,"К сожалению, в данной версии бота присутствует ограничение, равное 5 запросов в минуту",reply_markup=menu2())
+            return
+        res = 'Последнее обновление о тикере ' + id + ' от ' + data['Meta Data']['3. Last Refreshed'] + ' :\n'
+        res += 'Открытие:  '+ data['Time Series (1min)'][data['Meta Data']['3. Last Refreshed']]['1. open']+' USD\n'
+        res += 'Максимум:  '+ data['Time Series (1min)'][data['Meta Data']['3. Last Refreshed']]['2. high'] + ' USD\n'
+        res += 'Минимум:  '+ data['Time Series (1min)'][data['Meta Data']['3. Last Refreshed']]['3. low'] + ' USD\n'
+        res += 'Закрытие:  '+ data['Time Series (1min)'][data['Meta Data']['3. Last Refreshed']]['4. close'] + ' USD\n'
+        res += 'Объем торгов:  '+ data['Time Series (1min)'][data['Meta Data']['3. Last Refreshed']]['5. volume'] + '\n'
+        bot.send_message(message.chat.id, res, reply_markup=menu2())
     except:
-        bot.send_message(message.from_user.id, 'incorrect ticker. Try again')
-        bot.register_next_step_handler(message, send_ticker)
+        bot.send_message(message.chat.id, 'Введен некорректный тикер', reply_markup=menu2())
 
-def add_num_of_stocks(message,res):
+def add_num_of_stocks(message, res):
     f = open('text.txt', 'a')
     if ((message.text).isdigit()):
-            r = res+' '+message.text
-            f.write(r)
-            bot.send_message(message.from_user.id, 'good')
+        r = res + ' ' + message.text
+        f.write(r)
+        bot.send_message(message.chat.id, 'Данные добавлены в портфель',reply_markup=menu2())
     else:
-        bot.send_message(message.from_user.id,'incorrect num. Try again')
+        bot.send_message(message.chat.id, 'Некорректный ввод.',reply_markup=menu2())
+
+def get_count_of_stocks_case():
+    f = open("text.txt")
+    count=0
+    while (f.readline()):
+        count+=1
+    return count
+
+def clear_case():
+    if (os.stat("text.txt").st_size == 0): return
+    else:
+        f = open('text.txt', 'w').close()
+        return
+
+
 bot.polling(none_stop=True, interval=0)
